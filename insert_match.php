40db528e4b8de72490e21e9665fb86fa2f14afe7
Marcelo Almeida <marcelocralmeida@gmail.com>
2013-05-04 07:44:32 -0300

    playmatch php

--- insert_match.php
+++ insert_match.php
@@ -7,8 +7,9 @@
     $f2 = $_POST['f2'];
     $f3 = $_POST['f3'];
     $f4 = $_POST['f4'];
-    
-    $match_id = createMatch($user_id, $friend_id, $f1, $f2, $f3, $f4, $db);
+
+    $match_obj = createMatch($user_id, $friend_id, $f1, $f2, $f3, $f4, $db);
+    $match_id = $match_obj["lastval"];
     echo "/playmatch.php?match_id=".$match_id;
 
 ?>
--- playmatch.php
+++ playmatch.php
@@ -1,6 +1,12 @@
 <?php
 
-    
+    include 'database.php';
+    $match_id = $_GET['match_id'];
+    $match = getMatch($match_id, $db)["p2_id"]
+    $player = $match->p1_id
+    if($match->terminou1){
+        $player = p2_id;
+    }
     //dar um getMatch e verificar se eh a vez do primeiro ou do segundo player
     //setar alguma flag que diz de quem eh a vez
     //teremos uma variavel chamada $match
