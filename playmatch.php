dfb5bb943c60bbdb06114dd4045bcd69ae0bd732
Marcelo Almeida <marcelocralmeida@gmail.com>
2013-05-04 09:10:23 -0300

    integrando template com servidor php

--- playmatch.php
+++ playmatch.php
@@ -2,10 +2,10 @@
 
     include 'database.php';
     $match_id = $_GET['match_id'];
-    $match = getMatch($match_id, $db)["p2_id"]
-    $player = $match->p1_id
-    if($match->terminou1){
-        $player = p2_id;
+    $match = getMatch($match_id, $db);
+    $player = $match->p1_id;
+    if($match->terminou1==1){
+        $player = $match->p2_id;
     }
     //dar um getMatch e verificar se eh a vez do primeiro ou do segundo player
     //setar alguma flag que diz de quem eh a vez
